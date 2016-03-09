6626bb1ba7db1c0423813f65efd33a8842c94ae3
Elliotpwnz <Elliotsminion@gmail.com>
2016-03-09 16:51:43 -0500

    portal direction + more portals
    added portals for every room and positioned them
    added portal direction system

--- Assets/Scripts/roomController.js
+++ Assets/Scripts/roomController.js
@@ -1,9 +1,18 @@
 ﻿#pragma strict
 
 public var roomID : int;
+public var bottomSpawn : Vector3;
+public var topSpawn : Vector3;
+public var leftSpawn : Vector3;
+public var rightSpawn : Vector3;
 
 function Start () {
-
+	//Note: The float values should not be hardcoded and should be calculated based on the size of the room.
+	//Also note that the -8 is the current Z-value of the spider.
+	bottomSpawn = new Vector3(transform.position.x, transform.position.y - .5f, -8);
+	topSpawn = new Vector3(transform.position.x, transform.position.y + .5f, -8);
+	leftSpawn = new Vector3(transform.position.x - 1.5f, transform.position.y, -8);
+	rightSpawn = new Vector3(transform.position.x + 1.5f, transform.position.y, -8);
 }
 
 function Update () {
